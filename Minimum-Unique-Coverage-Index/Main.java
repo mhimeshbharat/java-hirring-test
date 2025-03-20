e77b522abce6e74123668af7b02f49a4319b40e0
mhimeshbharat <mudumbavenkata.himeshbharat@amadeus.com>
2025-03-20 14:21:24 +0530

    Update Main.java

--- Minimum-Unique-Coverage-Index/Main.java
+++ Minimum-Unique-Coverage-Index/Main.java
@@ -5,7 +5,10 @@ public class Main {
         return 0;// TODO implement this
   }
   public static void main(String[] args) {
-        System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1, 5, 3, 7, 3, 7, 5, 1)));
+        System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(3,3)));
+	System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(2,5,2)));
+	System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1,0,6)));
+	System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1, 5, 3, 7, 3, 7, 5, 1)));
         System.out.println("Test 2: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1, 5, 3, 7, 3, 7, 5, 1, 9, 2, 4, 6, 8, 0, 9, 2, 4, 6, 8, 0)));
   }
 }

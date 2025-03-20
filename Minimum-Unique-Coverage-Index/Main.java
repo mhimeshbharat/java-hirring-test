8eace08c57b87e6687e7ad5fe525d11aafde343e
mhimeshbharat <mudumbavenkata.himeshbharat@amadeus.com>
2025-03-20 14:23:26 +0530

    Update Main.java

--- Minimum-Unique-Coverage-Index/Main.java
+++ Minimum-Unique-Coverage-Index/Main.java
@@ -6,9 +6,9 @@ public class Main {
   }
   public static void main(String[] args) {
         System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(3,3)));
-	System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(2,5,2)));
-	System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1,0,6)));
-	System.out.println("Test 1: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1, 5, 3, 7, 3, 7, 5, 1)));
-        System.out.println("Test 2: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1, 5, 3, 7, 3, 7, 5, 1, 9, 2, 4, 6, 8, 0, 9, 2, 4, 6, 8, 0)));
+	System.out.println("Test 2: Least index covering all unique values: " + findLeastIndex(Arrays.asList(2,5,2)));
+	System.out.println("Test 3: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1,0,6)));
+	System.out.println("Test 4: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1, 5, 3, 7, 3, 7, 5, 1)));
+        System.out.println("Test 5: Least index covering all unique values: " + findLeastIndex(Arrays.asList(1, 5, 3, 7, 3, 7, 5, 1, 9, 2, 4, 6, 8, 0, 9, 2, 4, 6, 8, 0)));
   }
 }
